6a7be00a2c2343d0030ab425815820d341a62c8f
shajjad-test <shajjad.test123@gmail.com>
2021-11-22 15:07:19 +0600

    about ends

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
-    $('nav .nav-list ul li').click(function(){
-        $(this).addClass('active').siblings().removeClass('active');
+    //banner slider;
+    $('.banner-slider').slick({
+        arrows: false,
+        dots: true,
     });
 
-    $("#vdo").YTPlayer();
+    //about video popup
+    $('.venobox').venobox();
 });
